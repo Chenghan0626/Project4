99d0f188f5b727e46efe1c436b6393a1e337841a
Chenghan0626 <b2209336@ulive.pccu.edu.tw>
2024-06-18 10:06:01 +0800

    加入專案檔案。

---
+++ Project4/CardTypeEvaluator.h
@@ -0,0 +1,27 @@
+#ifndef CARDTYPEEVALUATOR_H
+#define CARDTYPEEVALUATOR_H
+
+struct Card {
+    int value;
+    char suit;
+};
+
+class CardTypeEvaluator {
+public:
+    static void sortHand(Card* hand);
+    static const char* evaluateHand(Card* hand);
+    static int getHandRank(const char* type);
+
+private:
+    static bool isRoyalFlush(const Card* hand);
+    static bool isStraightFlush(const Card* hand);
+    static bool isFourOfAKind(const Card* hand);
+    static bool isFullHouse(const Card* hand);
+    static bool isFlush(const Card* hand);
+    static bool isStraight(const Card* hand);
+    static bool isThreeOfAKind(const Card* hand);
+    static bool isTwoPair(const Card* hand);
+    static bool isPair(const Card* hand);
+};
+
+#endif
